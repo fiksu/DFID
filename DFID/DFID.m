8df41d9e34db69212255238ff60b26a0053b60bf
Mark Corner <mcorner@gmail.com>
2014-02-10 21:46:54 -0500

    Initial Commit

---
+++ DFID/DFID.m
@@ -0,0 +1,13 @@
+//
+//  DFID.m
+//  DFID
+//
+//  Created by Mark Corner on 2/10/14.
+//  Copyright (c) 2014 Fiksu Inc. All rights reserved.
+//
+
+#import "DFID.h"
+
+@implementation DFID
+
+@end
---
+++ DFIDTests/DFIDTests.m
@@ -0,0 +1,34 @@
+//
+//  DFIDTests.m
+//  DFIDTests
+//
+//  Created by Mark Corner on 2/10/14.
+//  Copyright (c) 2014 Fiksu Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DFIDTests : XCTestCase
+
+@end
+
+@implementation DFIDTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
